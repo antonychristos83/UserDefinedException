3dd357e75a5e85124da15350c241385f38bd3633
antonychristos83 <antonychristos83@gmail.com>
2022-07-08 10:17:30 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UserDefinedException
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter age : ");
+            int age = Convert.ToInt32(Console.ReadLine());
+            checkAge(age);
+        }
+        static void checkAge(int age)
+        {
+            if (age < 18)
+            {
+                throw new ArithmeticException("Access Denied");
+            }
+            else
+            {
+                Console.WriteLine("Access Granted");
+            }
+        }
+    }
+}
